9aa92d6347069109300d9e56852645e0b47145e6
kazimayzid <kazimayzid208@gmail.com>
2025-12-29 23:52:17 +0600

    project started and almost 50% done

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import "./App.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Socks", quantity: 12, packed: false },
+];
+export default function App() {
+  return (
+    <>
+      <div className="app">
+        <Logo />
+        <Form />
+        <PackingList />
+        <Stats />
+      </div>
+    </>
+  );
+}
+
+
+
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+
+
+
+function Form() {
+ function handleSubmit(e) {
+  e.preventDefault()
+ }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="add-form">
+        <h3>What do you need for your 😍 trip?</h3>
+        <select name="" id="">
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option key={num} value={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input type="text" placeholder="Item...." />
+        <button>Add</button>
+      </form>
+    </>
+  );
+}
+
+
+
+
+
+function PackingList() {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {initialItems.map((item) => (
+            <Item key={item.id} item={item} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+
+
+
+
+function Item({ item }) {
+  return (
+    <>
+      <li>
+        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+          {item.quantity} {item.description}
+        </span>
+        <button>❌</button>
+      </li>
+    </>
+  );
+}
+
+
+
+
+function Stats() {
+  return (
+    <>
+      <footer className="stats">
+        <em>👜 You have X items on your list, and you already packed X (X%)</em>
+      </footer>
+    </>
+  );
+}
